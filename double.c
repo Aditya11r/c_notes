297cc09176f5563d705eb27ac27a5de00a82e30e
aditya raj <adikochas@gmail.com>
2023-06-11 00:53:48 +0530

    changes

--- double.c
+++ double.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 int main()
-{   int x,y=0,m=2;
-    int num[10];
-    printf("enter number of values you want in your array(under 10):");
+{   
+    long x,y=0,m=2;
+    int num[100];
+    do
+    {
+    printf("enter number of values you want in your array(under 100):");
     scanf("%i",&x);
+    }
+    while(x<1);
     while(y<x)
     {
        printf("the walue of %i element is %i\n",y+1,m);
